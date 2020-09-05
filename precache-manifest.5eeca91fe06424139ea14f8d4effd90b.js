f208bf0e9c54fa35c4fe8f931e955cd57c1cb5fd
Waqas Ali <arien.online@yahoo.com>
2020-09-05 21:38:26 +0200

    Updates

---
+++ precache-manifest.5eeca91fe06424139ea14f8d4effd90b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c344a55b89f0ad86aa7d61e2a2da729",
+    "url": "/Tac/index.html"
+  },
+  {
+    "revision": "3eb9aa3f0044e17d5287",
+    "url": "/Tac/static/js/2.842ab24f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tac/static/js/2.842ab24f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6240c2ad44664f31a898",
+    "url": "/Tac/static/js/main.1edcae60.chunk.js"
+  },
+  {
+    "revision": "9d01eaf5ee177d2ac7b1",
+    "url": "/Tac/static/js/runtime-main.58896ef9.js"
+  }
+]);
